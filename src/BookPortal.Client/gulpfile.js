3dd928ddc39b0e99559e163b832aef93a373d792
ravengerUA <qe1dg7bm>
2015-05-11 21:09:31 +0300

    added BookPortal client

---
+++ src/BookPortal.Client/gulpfile.js
@@ -0,0 +1,28 @@
+﻿/// <binding Clean='clean' />
+
+var gulp = require("gulp"),
+  rimraf = require("rimraf"),
+  fs = require("fs");
+
+eval("var project = " + fs.readFileSync("./project.json"));
+
+var paths = {
+    bower: "./bower_components/",
+    lib: "./" + project.webroot + "/lib/"
+};
+
+gulp.task("clean", function (cb) {
+    rimraf(paths.lib, cb);
+});
+
+gulp.task("copy", ["clean"], function () {
+    var bower = {
+        "jquery": "jquery*.{js,map}",
+        "angular": "angular*.{js,map}"
+    }
+
+    for (var destinationDir in bower) {
+        gulp.src(paths.bower + bower[destinationDir])
+          .pipe(gulp.dest(paths.lib + destinationDir));
+    }
+});
---
+++ src/BookPortal.Client/wwwroot/scripts/controllers.js
@@ -0,0 +1,52 @@
+﻿'use strict';
+
+/* Controllers */
+var awardCatApp = angular.module('awardCatApp', []);
+
+function getImagesUrl() {
+    return 'http://data.fantlab.org/images';
+}
+
+function getDefaultAwardSort() {
+    return 'lang';
+}
+
+function Award(data) {
+    this.awardId = data.id;
+    this.rusname = data.rusname;
+    this.name = data.name;
+    this.countryId = data.country.id;
+    this.countryName = data.country.name;
+    this.awardClose = data.isclosed === 1 ? getImagesUrl() + '/sclosed.gif' : getImagesUrl() + '/sclosedna.gif';
+    this.awardType = data.awardtype === 1 ? 'общелитературная' : 'фантастическая';
+    //this.nominationsCount = data.nomi_count;
+    //this.contestsCount = data.contests_count;
+    //this.minDate = data.min_date.replace(/-\d{2}-\d{2}/, "");
+    //this.maxDate = data.max_date.replace(/-\d{2}-\d{2}/, "");
+    this.awardUrl = "/award" + data.award_id;
+    this.iconScr = getImagesUrl() + '/awards/icons/' + data.id + '_icon';
+    this.countryImageScr = getImagesUrl() + '/flags/' + data.country.id + ".png";
+}
+
+awardCatApp.controller('AwardListCtrl', ['$scope', '$http', function awardListCtrl($scope, $http) {
+    function loadDataFromService(sort) {
+        $http.get('http://localhost:5000/api/awards?sort=' + sort).success(function (data) {
+            $scope.awards = $.map(data.result.rows, function (item) { return new Award(item.values); });
+        });
+    }
+
+    loadDataFromService(getDefaultAwardSort());
+
+    $scope.sortTypes = [
+      { sortName: "по названию", sortValue: "name" },
+      { sortName: "по стране", sortValue: "country" },
+      { sortName: "по типу", sortValue: "type" },
+      { sortName: "по языку", sortValue: "lang" },
+    ];
+
+    $scope.currentSort = $scope.sortTypes[3];
+
+    $scope.changeSort = function () {
+        loadDataFromService($scope.currentSort.sortValue);
+    }
+}]);
