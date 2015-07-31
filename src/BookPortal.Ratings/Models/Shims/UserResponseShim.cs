cdd52a185dea3db25969d5b06c7163782cb24cd1
ravengerUA <qe1dg7bm>
2015-07-31 18:25:59 +0300

    removed Ratings Service

--- src/BookPortal.Ratings/Models/Shims/UserResponseShim.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookPortal.Ratings.Models.Shims
-{
-    public class UserResponseShim
-    {
-        public int UserId { get; set; }
-
-        public string Name { get; set; }
-    }
-}
