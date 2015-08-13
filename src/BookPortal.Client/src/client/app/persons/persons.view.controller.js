20a37298b6a689ae4812e934b53fa6a25fd8ce20
ravengerUA <yurisikorsky@gmail.com>
2015-08-13 09:21:09 +0300

    updated personwork directive

--- src/BookPortal.Client/src/client/app/persons/persons.view.controller.js
+++ src/BookPortal.Client/src/client/app/persons/persons.view.controller.js
@@ -38,7 +38,56 @@
 
         function getPersonWorks(personId) {
             return dataservice.getPersonWorks(personId).then(function (data) {
-                vm.person.works = _.groupBy(data, function (item) { return item.worktypelevel; });
+                var mockData = {
+                    "6": [
+                        {
+                            "workid": 1,
+                            "rusname": "Гиперион",
+                            "name": "Hyperion",
+                            "year": 1989,
+                            "votescount": 4615, // TODO
+                            "rating": 8.81, // TODO
+                            "usermark": 8, // TODO
+                            "root_cycle_work_id": 92, // TODO
+                            "root_cycle_work_name": "Песни Гипериона", // TODO
+                            "root_cycle_work_type_id": 13 // TODO
+                        },
+                        {
+                            "workid": 603,
+                            "rusname": "Горящий Эдем",
+                            "name": "Fires of Eden",
+                            "altname": "Костры Эдема",
+                            "year": 1994,
+                            "votescount": 225,
+                            "rating": 6.89
+                        }
+                    ],
+                    "8": [
+                        {
+                            "workid": 33891,
+                            "name": "Presents of Mind",
+                            "year": 1985,
+                            "persons": [
+                                {
+                                    "personid": 396,
+                                    "name": "Эдвард Брайант",
+                                    "persontype": "author"
+                                },
+                                {
+                                    "personid": 788,
+                                    "name": "Конни Уиллис",
+                                    "persontype": "author"
+                                }
+                            ],
+                            "coauthortype": "coauthor", // TODO: coauthor | author | editor | macycle
+                            "votescount": 1,
+                            "rating": 7
+                        }
+                    ]
+                }
+
+                vm.person.works = mockData;
+                // vm.person.works = _.groupBy(data, function (item) { return item.worktypelevel; });
                 return vm.person.works;
             });
         }
